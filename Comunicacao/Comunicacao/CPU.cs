c0046d9cb53531f710c3dc96e7c698497de38ab7
RodrigoSource <ralmeida250@gmail.com>
2025-03-30 16:25:26 -0300

    CPU

---
+++ Comunicacao/Comunicacao/CPU.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace Comunicacao
+{
+    public class CPU : Recurso
+    {
+        public double Ocupacao { get; private set; }
+
+        public CPU(double cpuTotal) : base(0, cpuTotal)
+        {
+            Ocupacao = 0;
+        }
+
+        public override void Alocation(int quantidade)
+        {
+            Random rand = new Random();
+            Ocupacao = rand.NextDouble() * 100; // Randomiza a ocupação da CPU entre 0% e 100%
+
+            if (Ocupacao <= quantidade)
+            {
+                Disponibilidade = true;
+                Console.WriteLine($"CPU alocada com uso de {Ocupacao:F2}%.");
+            }
+            else
+            {
+                Disponibilidade = false;
+                Console.WriteLine("Falha na alocação da CPU. Uso excedente.");
+            }
+        }
+    }
+}
+
---
+++ Comunicacao/Comunicacao/Recurso.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+
+namespace Comunicacao
+{
+    public abstract class Recurso
+    {
+        public double MemoriaTotal { get; protected set; } //Define Total de Memoria ram
+        public double CPUTotal { get; protected set; } //Define Total de CPU
+        public bool Disponibilidade { get; protected set; } //Consulta de há disponibilidade de recurso e me retorna se verdadeiro ou falso
+
+        protected Dictionary<int, string> PosicoesMemoria;  // Mapeamento da posição para endereço
+
+        public Recurso(double memoriaTotal, double cpuTotal)
+        {
+            MemoriaTotal = memoriaTotal;
+            CPUTotal = cpuTotal;
+            Disponibilidade = true;
+            PosicoesMemoria = new Dictionary<int, string>();
+        }
+
+        public abstract void Alocation(int quantidade);
+
+        public bool ConsultarDisponibilidade(int quantidade)
+        {
+            return MemoriaTotal >= quantidade;
+        }
+
+        public void DefineMemoriaTotal(int quantidade)
+        {
+            MemoriaTotal = quantidade;
+            ConsultaPosicoes(quantidade);
+            Console.WriteLine($"Memória total definida para {quantidade} posições.");
+        }
+
+        public void ConsultaPosicoes(int quantidade)
+        {
+            PosicoesMemoria.Clear();
+            for (int i = 0; i < quantidade; i++)
+            {
+                string endereco = $"0x{i.ToString("X4")}";
+                PosicoesMemoria.Add(i, endereco);
+            }
+            Console.WriteLine($"Memória inicializada com {quantidade} posições.");
+        }
+
+        public void DefineCPU(int quantidade)
+        {
+            Random rand = new Random();
+            CPUTotal = rand.Next(1, quantidade + 1);
+            Console.WriteLine($"CPU inicializada com {CPUTotal}% de uso.");
+        }
+    }
+}
